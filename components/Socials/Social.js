7b75eef7f1562d45ebbc51d4a3bde75097b15cb1
Syed Mohammad Younis <younushussain0@gmail.com>
2023-04-05 12:20:12 +0500

    comitt on 5-4-23

--- components/Socials/Social.js
+++ components/Socials/Social.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 import { AiFillTwitterCircle , AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import styles from './social.module.css'
@@ -6,9 +7,12 @@ import styles from './social.module.css'
 const social = () => {
   return (
     <div className={styles.social}>
-      <AiFillTwitterCircle className={styles.tweet} />
-      <FaLinkedinIn className={styles.linkd} />
-<AiFillGithub className={styles.git}  />
+     
+     <Link href='https://mobile.twitter.com/younushussain_9'><AiFillTwitterCircle className={styles.tweet} /></Link>
+
+
+  <Link href='https://www.linkedin.com/mwlite/in/younus-hussain-3357411b2'><FaLinkedinIn className={styles.linkd} /></Link>
+<Link href='https://github.com/YounusHussain9'><AiFillGithub className={styles.git}  /></Link>
     </div>
   )
 }
